aaa1a40e905df39728008733adb6d2bf24aefd19
Mauro Venticinque <mauro.venticinque@hotmail.com>
2025-04-21 12:20:08 -0300

    Add payment error page with error handling and user feedback

--- app/campaigns/payment-error/page.tsx
+++ app/campaigns/payment-error/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { Suspense } from 'react';
 import Link from 'next/link';
 import { useSearchParams } from 'next/navigation';
 import Header from '@/components/layout/Header';
